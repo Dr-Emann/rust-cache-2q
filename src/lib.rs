247b2e704dd86e77c89416aec39b32e3e1a31e22
Zachary Dremann <dremann@gmail.com>
2017-07-22 15:22:35 -0400

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -36,42 +36,44 @@ impl<'a, K, V> Into<(&'a K, &'a mut V)> for &'a mut CacheEntry<K, V> {
 }
 
 /// A 2q Cache which maps keys to values
-/// 
+///
 /// This cache based on the paper entitled
 /// **[2Q: A Low Overhead High-Performance Buffer Management Replacement Algorithm](http://www.vldb.org/conf/1994/P439.PDF)**.
 ///
-/// The cache is split into 3 sections, A1in, A1out and Am. A1in contains the most recently added entries.
+/// The cache is split into 3 sections, A1in, A1out and Am.
+/// A1in contains the most recently added entries.
 /// Am is an LRU cache which contains entries which are frequently accessed
 /// A1out contains the keys which have been recently evicted from the A1in cache.
-/// 
+///
 /// New entries in the cache are initially placed in A1in.
 /// After A1in fills up, the oldest entry from A1in will be removed, and its key is placed in A1out.
-/// When an entry is requested and not found, but its key is found in A1out, an entry is pushed to the front of Am.
+/// When an entry is requested and not found, but its key is found in A1out,
+/// an entry is pushed to the front of Am.
 ///
 /// # Examples
 ///
 /// ```
 /// use cache_2q::Cache;
-/// 
+///
 /// // type inference lets us omit an explicit type signature (which
 /// // would be `Cache<&str, &str>` in this example).
 /// let mut book_reviews = Cache::new(1024);
-/// 
+///
 /// // review some books.
 /// book_reviews.insert("Adventures of Huckleberry Finn",    "My favorite book.");
 /// book_reviews.insert("Grimms' Fairy Tales",               "Masterpiece.");
 /// book_reviews.insert("Pride and Prejudice",               "Very enjoyable.");
 /// book_reviews.insert("The Adventures of Sherlock Holmes", "Eye lyked it alot.");
-/// 
+///
 /// // check for a specific one.
 /// if !book_reviews.contains_key("Les Misérables") {
 ///     println!("We've got {} reviews, but Les Misérables ain't one.",
 ///              book_reviews.len());
 /// }
-/// 
+///
 /// // oops, this review has a lot of spelling mistakes, let's delete it.
 /// book_reviews.remove("The Adventures of Sherlock Holmes");
-/// 
+///
 /// // look up the values associated with some keys.
 /// let to_find = ["Pride and Prejudice", "Alice's Adventure in Wonderland"];
 /// for book in &to_find {
@@ -80,7 +82,7 @@ impl<'a, K, V> Into<(&'a K, &'a mut V)> for &'a mut CacheEntry<K, V> {
 ///         None => println!("{} is unreviewed.", book)
 ///     }
 /// }
-/// 
+///
 /// // iterate over everything.
 /// for (book, review) in &book_reviews {
 ///     println!("{}: \"{}\"", book, review);
@@ -89,27 +91,27 @@ impl<'a, K, V> Into<(&'a K, &'a mut V)> for &'a mut CacheEntry<K, V> {
 ///
 /// Cache also implements an Entry API, which allows for more complex methods of getting,
 /// setting, updating and removing keys and their values:
-/// 
+///
 /// ```
 /// use cache_2q::Cache;
-/// 
+///
 /// // type inference lets us omit an explicit type signature (which
 /// // would be `Cache<&str, u8>` in this example).
 /// let mut player_stats = Cache::new(32);
-/// 
+///
 /// fn random_stat_buff() -> u8 {
 ///     // could actually return some random value here - let's just return
 ///     // some fixed value for now
 ///     42
 /// }
-/// 
+///
 /// // insert a key only if it doesn't already exist
 /// player_stats.entry("health").or_insert(100);
-/// 
+///
 /// // insert a key using a function that provides a new value only if it
 /// // doesn't already exist
 /// player_stats.entry("defence").or_insert_with(random_stat_buff);
-/// 
+///
 /// // update a key, guarding against the key possibly not being set
 /// let stat = player_stats.entry("attack").or_insert(100);
 /// *stat += random_stat_buff();
@@ -128,7 +130,8 @@ impl<K: Eq, V> Cache<K, V> {
     /// Creates an empty cache, with the specified size
     ///
     /// # Notes
-    /// `size` defines the maximum number of entries, but there can be an additional `size / 2` instances of `K`
+    /// `size` defines the maximum number of entries, but there can be
+    /// an additional `size / 2` instances of `K`
     ///
     /// # Examples
     ///
@@ -170,33 +173,37 @@ impl<K: Eq, V> Cache<K, V> {
     /// assert_eq!(cache.contains_key(&2), false);
     /// ```
     pub fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
-        where K: Borrow<Q>,
-              Q: Eq
+    where
+        K: Borrow<Q>,
+        Q: Eq,
     {
         self.a1_in.iter().any(|entry| entry.key.borrow() == key) ||
             self.am.iter().any(|entry| entry.key.borrow() == key)
     }
 
     /// Returns a reference to the value corresponding to the key.
-    /// 
+    ///
     /// The key may be any borrowed form of the cache's key type, but Eq on the borrowed form
     /// must match those for the key type.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut map = Cache::new(32);
     /// map.insert(1, "a");
     /// assert_eq!(map.get(&1), Some(&"a"));
     /// assert_eq!(map.get(&2), None);
     /// ```
     pub fn get<Q: ?Sized>(&mut self, key: &Q) -> Option<&V>
-        where K: Borrow<Q>,
-              Q: Eq
+    where
+        K: Borrow<Q>,
+        Q: Eq,
     {
-        if let Some(&CacheEntry{ref value, ..}) = self.a1_in.iter().find(|entry| entry.key.borrow() == key) {
+        if let Some(&CacheEntry { ref value, .. }) =
+            self.a1_in.iter().find(|entry| entry.key.borrow() == key)
+        {
             Some(value)
         } else if let Some(i) = self.am.iter().position(|entry| entry.key.borrow() == key) {
             let old = self.am.remove(i).unwrap();
@@ -226,10 +233,14 @@ impl<K: Eq, V> Cache<K, V> {
     /// assert_eq!(cache.get(&1), Some(&"b"));
     /// ```
     pub fn get_mut<Q: ?Sized>(&mut self, key: &Q) -> Option<&mut V>
-        where K: Borrow<Q>,
-              Q: Eq
+    where
+        K: Borrow<Q>,
+        Q: Eq,
     {
-        if let Some(&mut CacheEntry{ref mut value, ..}) = self.a1_in.iter_mut().find(|entry| entry.key.borrow() == key) {
+        if let Some(&mut CacheEntry { ref mut value, .. }) = self.a1_in
+            .iter_mut()
+            .find(|entry| entry.key.borrow() == key)
+        {
             Some(value)
         } else if let Some(i) = self.am.iter().position(|entry| entry.key.borrow() == key) {
             let old = self.am.remove(i).unwrap();
@@ -245,7 +256,7 @@ impl<K: Eq, V> Cache<K, V> {
     /// If the cache did not have this key present, None is returned.
     ///
     /// If the cache did have this key present, the value is updated, and the old
-    /// value is returned. 
+    /// value is returned.
     ///
     /// # Examples
     ///
@@ -286,7 +297,12 @@ impl<K: Eq, V> Cache<K, V> {
     /// ```
     pub fn entry(&mut self, key: K) -> Entry<K, V> {
         let mut entry = self.peek_entry(key);
-        if let Entry::Occupied(OccupiedEntry { ref mut cache, kind: OccupiedKind::Frequent(ref mut i), ..}) = entry {
+        if let Entry::Occupied(OccupiedEntry {
+            ref mut cache,
+            kind: OccupiedKind::Frequent(ref mut i),
+            ..
+        }) = entry
+        {
             let old_entry = cache.am.remove(*i).unwrap();
             cache.am.push_front(old_entry);
             *i = 0;
@@ -295,12 +311,12 @@ impl<K: Eq, V> Cache<K, V> {
     }
 
     /// Returns the number of entries currenly in the cache.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut a = Cache::new(8);
     /// assert_eq!(a.len(), 0);
     /// a.insert(1, "a");
@@ -311,12 +327,12 @@ impl<K: Eq, V> Cache<K, V> {
     }
 
     /// Returns true if the map contains no elements.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut a = Cache::new(8);
     /// assert!(a.is_empty());
     /// a.insert(1, "a");
@@ -344,10 +360,14 @@ impl<K: Eq, V> Cache<K, V> {
     /// assert_eq!(cache.remove(&1), None);
     /// ```
     pub fn remove<Q: ?Sized>(&mut self, key: &Q) -> Option<V>
-        where K: Borrow<Q>,
-              Q: Eq,
+    where
+        K: Borrow<Q>,
+        Q: Eq,
     {
-        if let Some(i) = self.a1_in.iter().position(|entry| entry.key.borrow() == key) {
+        if let Some(i) = self.a1_in
+            .iter()
+            .position(|entry| entry.key.borrow() == key)
+        {
             Some(self.a1_in.remove(i).unwrap().value)
         } else if let Some(i) = self.am.iter().position(|entry| entry.key.borrow() == key) {
             Some(self.am.remove(i).unwrap().value)
@@ -357,12 +377,12 @@ impl<K: Eq, V> Cache<K, V> {
     }
 
     /// Clears the cache, removing all key-value pairs. Keeps the allocated memory for reuse.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut a = Cache::new(32);
     /// a.insert(1, "a");
     /// a.clear();
@@ -391,23 +411,23 @@ impl<K: Eq, V> Cache<K, V> {
     /// ```
     pub fn peek_entry(&mut self, key: K) -> Entry<K, V> {
         if let Some(i) = self.am.iter().position(|entry| &entry.key == &key) {
-            Entry::Occupied(OccupiedEntry{
+            Entry::Occupied(OccupiedEntry {
                 cache: self,
                 kind: OccupiedKind::Frequent(i),
             })
         } else if let Some(i) = self.a1_in.iter().position(|entry| &entry.key == &key) {
-            Entry::Occupied(OccupiedEntry{
+            Entry::Occupied(OccupiedEntry {
                 cache: self,
                 kind: OccupiedKind::Recent(i),
             })
         } else if let Some(i) = self.a1_out.iter().position(|old_key| old_key == &key) {
-            Entry::Vacant(VacantEntry{
+            Entry::Vacant(VacantEntry {
                 cache: self,
                 key: key,
                 kind: VacantKind::Remembered(i),
             })
         } else {
-            Entry::Vacant(VacantEntry{
+            Entry::Vacant(VacantEntry {
                 cache: self,
                 key: key,
                 kind: VacantKind::Unknown,
@@ -434,7 +454,7 @@ impl<K: Eq, V> Cache<K, V> {
     /// ```
     pub fn iter(&self) -> Iter<K, V> {
         Iter {
-            inner: self.a1_in.iter().chain(self.am.iter()).map(Into::into)
+            inner: self.a1_in.iter().chain(self.am.iter()).map(Into::into),
         }
     }
 }
@@ -454,22 +474,14 @@ pub enum Entry<'a, K: 'a, V: 'a> {
     /// An occupied entry
     Occupied(OccupiedEntry<'a, K, V>),
     /// An vacant entry
-    Vacant(VacantEntry<'a, K, V>)
+    Vacant(VacantEntry<'a, K, V>),
 }
 
 impl<'a, K: 'a + fmt::Debug, V: 'a + fmt::Debug> fmt::Debug for Entry<'a, K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Entry::Vacant(ref v) => {
-                f.debug_tuple("Entry")
-                    .field(v)
-                    .finish()
-            }
-            Entry::Occupied(ref o) => {
-                f.debug_tuple("Entry")
-                    .field(o)
-                    .finish()
-            }
+            Entry::Vacant(ref v) => f.debug_tuple("Entry").field(v).finish(),
+            Entry::Occupied(ref o) => f.debug_tuple("Entry").field(o).finish(),
         }
     }
 }
@@ -498,13 +510,13 @@ impl<'a, K: 'a + Eq, V: 'a> Entry<'a, K, V> {
     /// # Examples
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut cache = Cache::new(8);
     /// {
     ///     let value = cache.entry(0xFF00).or_insert(0);
     ///     assert_eq!(*value, 0);
     /// }
-    /// 
+    ///
     /// *cache.entry(0xFF00).or_insert(100) += 1;
     /// assert_eq!(*cache.get(&0xFF00).unwrap(), 1);
     /// ```
@@ -522,10 +534,10 @@ impl<'a, K: 'a + Eq, V: 'a> Entry<'a, K, V> {
     ///
     /// ```
     /// use cache_2q::Cache;
-    /// 
+    ///
     /// let mut cache: Cache<&'static str, String> = Cache::new(8);
     /// cache.entry("key").or_insert_with(|| "value".to_string());
-    /// 
+    ///
     /// assert_eq!(cache.get(&"key").unwrap(), &"value".to_string());
     /// ```
     pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V {
@@ -551,7 +563,14 @@ impl<'a, K: 'a + fmt::Debug, V: 'a + fmt::Debug> fmt::Debug for OccupiedEntry<'a
         f.debug_struct("OccupiedEntry")
             .field("key", self.key())
             .field("value", self.get())
-            .field("kind", & if let OccupiedKind::Frequent(_) = self.kind { "frequent" } else { "recent" })
+            .field(
+                "kind",
+                &if let OccupiedKind::Frequent(_) = self.kind {
+                    "frequent"
+                } else {
+                    "recent"
+                },
+            )
             .finish()
     }
 }
@@ -708,7 +727,7 @@ impl<'a, K: 'a, V: 'a> OccupiedEntry<'a, K, V> {
             OccupiedKind::Recent(idx) => {
                 let entry = self.cache.a1_in.remove(idx).unwrap();
                 (entry.key, entry.value)
-            },
+            }
             OccupiedKind::Frequent(idx) => {
                 let entry = self.cache.am.remove(idx).unwrap();
                 (entry.key, entry.value)
@@ -754,7 +773,14 @@ impl<'a, K: 'a + fmt::Debug, V: 'a + fmt::Debug> fmt::Debug for VacantEntry<'a, 
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("VacantEntry")
             .field("key", self.key())
-            .field("remembered", & if let VacantKind::Remembered(_) = self.kind { true } else { false })
+            .field(
+                "remembered",
+                &if let VacantKind::Remembered(_) = self.kind {
+                    true
+                } else {
+                    false
+                },
+            )
             .finish()
     }
 }
@@ -819,7 +845,7 @@ impl<'a, K: 'a + Eq, V: 'a> VacantEntry<'a, K, V> {
     /// assert_eq!(*cache.get("poneyland").unwrap(), 37);
     /// ```
     pub fn insert(self, value: V) -> &'a mut V {
-        let VacantEntry{cache, key, kind} = self;
+        let VacantEntry { cache, key, kind } = self;
         match kind {
             VacantKind::Remembered(idx) => {
                 cache.a1_out.remove(idx);
@@ -831,7 +857,7 @@ impl<'a, K: 'a + Eq, V: 'a> VacantEntry<'a, K, V> {
                     value: value,
                 });
                 &mut cache.am[0].value
-            },
+            }
             VacantKind::Unknown => {
                 if cache.a1_in.len() + 1 > cache.k_in {
                     let old_key = cache.a1_in.pop_back().unwrap().key;
@@ -858,22 +884,25 @@ impl<'a, K: 'a + Eq, V: 'a> VacantEntry<'a, K, V> {
 /// [`iter`]: struct.Cache.html#method.iter
 /// [`Cache`]: struct.Cache.html
 pub struct Iter<'a, K: 'a, V: 'a> {
-    inner: iter::Map<iter::Chain<vec_deque::Iter<'a, CacheEntry<K, V>>, vec_deque::Iter<'a, CacheEntry<K, V>>>, fn(&'a CacheEntry<K, V>) -> (&K, &V)>
+    inner: iter::Map<
+        iter::Chain<vec_deque::Iter<'a, CacheEntry<K, V>>, vec_deque::Iter<'a, CacheEntry<K, V>>>,
+        fn(&'a CacheEntry<K, V>) -> (&K, &V),
+    >,
 }
 
 impl<'a, K: 'a, V: 'a> Clone for Iter<'a, K, V> {
     fn clone(&self) -> Self {
-        Iter { inner: self.inner.clone() }
+        Iter {
+            inner: self.inner.clone(),
+        }
     }
 }
 
-impl <'a, K: 'a + fmt::Debug, V: 'a + fmt::Debug> fmt::Debug for Iter<'a, K, V> {
+impl<'a, K: 'a + fmt::Debug, V: 'a + fmt::Debug> fmt::Debug for Iter<'a, K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_list()
-            .entries(self.clone())
-            .finish()
+        f.debug_list().entries(self.clone()).finish()
     }
-} 
+}
 
 impl<'a, K: 'a, V: 'a> Iterator for Iter<'a, K, V> {
     type Item = (&'a K, &'a V);
